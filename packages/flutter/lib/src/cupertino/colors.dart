34d8ddd7b51c7f3f37e1f9e68fe2eadba07b5a35
Michael Leonhard <michael@leonhardllc.com>
2021-05-05 01:26:29 -0700

    Fix CupertinoAlertDialog dark mode button borders
    See https://github.com/flutter/flutter/issues/80921 for details and screenshots.

--- packages/flutter/lib/src/cupertino/colors.dart
+++ packages/flutter/lib/src/cupertino/colors.dart
@@ -537,7 +537,7 @@ class CupertinoColors {
     highContrastColor: Color.fromARGB(94, 60, 60, 67),
     darkHighContrastColor: Color.fromARGB(173, 84, 84, 88),
     elevatedColor: Color.fromARGB(73, 60, 60, 67),
-    darkElevatedColor: Color.fromARGB(153, 84, 84, 88),
+    darkElevatedColor: Color.fromARGB(153, 210, 210, 210),
     highContrastElevatedColor: Color.fromARGB(94, 60, 60, 67),
     darkHighContrastElevatedColor: Color.fromARGB(173, 84, 84, 88),
   );
